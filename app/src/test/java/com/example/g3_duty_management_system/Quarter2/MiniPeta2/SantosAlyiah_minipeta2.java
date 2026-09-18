dc7412c6c82c6b697c98e306926e39da47480877
Akysantos9-sys <alyiahkymsantos9@gmail.com>
2026-09-18 20:18:38 +0800

    Added all minipeta2 made by members

---
+++ app/src/test/java/com/example/g3_duty_management_system/Quarter2/MiniPeta2/SantosAlyiah_minipeta2.java
@@ -0,0 +1,18 @@
+package com.example.g3_duty_management_system.Quarter2.MiniPeta2;
+import org.junit.Test;
+
+public class SantosAlyiah_minipeta2 {
+@Test
+public void printmyProfile() {
+    String myName = "Aky";
+    String petName = "Tilapia";
+    String favFood = "Adobo";
+    int myage = 17;
+
+    System.out.println("--- My Digital Profile ---");
+    System.out.println("Hello!, my name is " + myName + " and i am " + myage);
+    System.out.println("And I know a energetic cat named " + petName + ".");
+    System.out.println("And one of my favorite food is " + favFood + ".");
+}
+    }
+
---
+++ app/src/test/java/com/example/g3_duty_management_system/Quarter2/MiniPeta2/SorianoZijan_minipeta2.java
@@ -0,0 +1,17 @@
+package com.example.g3_duty_management_system.Quarter2.MiniPeta2;
+import org.junit.Test;
+
+public class SorianoZijan_minipeta2 {
+@Test
+public void printmyProfile() {
+    String myName = "Zijan";
+    String petName = "Kimchi";
+    String favFood = "Fried chicken";
+    int myage = 17;
+
+    System.out.println("--- My Digital Profile ---");
+    System.out.println("Hello!, my name is " + myName + " and i am " + myage);
+    System.out.println("And I have a energetic dog named " + petName + ".");
+    System.out.println("And one of my favorite food is " + favFood + ".");
+}
+    }
