040da0cf70d3e5a2b5799f28d761c8b30f569337
MarkLimcuando <2015-60177@mcstayuman.edu.ph>
2026-09-18 21:33:22 +0800

    minipeta limcuando

---
+++ app/src/test/java/com/example/g3_duty_management_system/Quarter2/MiniPeta2/Limcuando_minipeta2.java
@@ -0,0 +1,22 @@
+package com.example.g3_duty_management_system.Quarter2.MiniPeta2;
+
+import org.junit.Test;
+public class Limcuando_minipeta2 {
+@Test
+public void main() {
+
+    String name = "Mark Christian Limcuando";
+    int age = 17;
+    String hobby = "Playing video games and listening to music";
+    String favoriteFood = "Nuggets";
+    String goal = "My goal in right now is to graduate.\n";
+
+    System.out.println("===== ABOUT ME =====");
+    System.out.println("Name: " + name);
+    System.out.println("Age: " + age);
+    System.out.println("Hobby: " + hobby);
+    System.out.println("Favorite Food: " + favoriteFood);
+    System.out.println("My Goal: " + goal);
+    System.out.println("====================");
+}
+}
