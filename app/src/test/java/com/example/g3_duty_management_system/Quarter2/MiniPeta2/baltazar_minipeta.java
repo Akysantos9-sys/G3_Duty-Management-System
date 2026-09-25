4613359a4aa2d93dd2b7244d41f3781899828869
jazz <danieljazzmontilano@gmail.com>
2026-09-25 10:50:08 +0800

    baltazar

---
+++ app/src/test/java/com/example/g3_duty_management_system/Quarter2/MiniPeta2/baltazar_minipeta.java
@@ -0,0 +1,18 @@
+package com.example.g3_duty_management_system.Quarter2.MiniPeta2;
+
+import org.junit.Test;
+
+public class baltazar_minipeta {
+   @Test
+public void myProfileInfo() {
+    String myName = "Vincent Baltazar";
+    String favGame = "Minecraft";
+    String favFood = "Spaghetti";
+    int Age = 17;
+
+    System.out.println("--- MY DIGITAL PROFILE ---");
+    System.out.println("Hello! my name is " + myName + ", I am currently " + Age + " years old.");
+    System.out.println("My favorite game to play is " + favGame + ".");
+    System.out.println("To hit my protein intake, " + favFood + " gets me through the day!");
+}
+}
